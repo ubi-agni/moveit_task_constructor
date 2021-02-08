ba1464c6545c63b29197a0909ea2a2f3d4582ca9
cpetersmeier <cpetersmeier@techfak.uni-bielefeld.de>
2021-02-08 09:06:10 +0100

    rostest_trampoline: assert virtual method calls
        - mock.patch virtual methods to assert
          correct insertion into the task hierarchy

--- core/python/test/rostest_trampoline.py
+++ core/python/test/rostest_trampoline.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import unittest
+import mock
 import rostest
 from moveit.python_tools import roscpp_init
 from moveit.task_constructor import core, stages
@@ -13,12 +14,15 @@ import copy
 
 
 class TestTrampolines(unittest.TestCase):
-    """ Test the functionality of pybind11 trampoline classes.
+    """ Test the functionality of trampoline classes.
     - Python classes are able to inherit from C++ base classes, which are
       exposed to python through trampoline classes and bindings.
     - Test for overriding of virtual inheritance of member functions
       and correct construction of the classes.
     """
+
+    def setUp(self):
+        roscpp_init("test_mtc")
 
     def check(self, test_class, **kwargs):
         """Test a trampoline class.
@@ -53,21 +57,11 @@ class TestTrampolines(unittest.TestCase):
             if "name" in kwargs:
                 self.assertEqual(test_class.name, kwargs.get("name"))
 
-        # perform checks
+        # actually call the checks
         _checkParameters(test_class)
         _checkBases(test_class)
 
     def test_connecting(self):
-        def checkInTask(test_class):
-            """ Checks correct insertion into the mtc task hierarchy.
-            - The @p test_class should be able to connect two generator stages.
-            """
-            task = core.Task()
-            task.add(stages.CurrentState("current"))
-            task.add(test_class)
-            task.add(stages.CurrentState("current"))
-            task.plan()
-
         class extConnecting(core.Connecting):
             """ Implements a 'Connecting' stage
             """
@@ -76,72 +70,67 @@ class TestTrampolines(unittest.TestCase):
                 core.Connecting.__init__(self, name)
 
             def compute(self, from_state, to_state):
-                print('compute called')
+                pass
 
         kwargs = {'name': 'Connecting'}
-        conn = extConnecting(kwargs.get('name'))
+        connecting = extConnecting(kwargs.get('name'))
 
         # check parameters and base classes
-        self.check(conn, **kwargs)
+        self.check(connecting, **kwargs)
 
-        # check task insertion
-        checkInTask(conn)
+        with mock.patch.object(extConnecting, 'compute') as compute:
+            task = core.Task()
+            task.add(stages.CurrentState("current"))
+            task.add(connecting)
+            task.add(stages.CurrentState("current"))
+            task.plan()
+            task.reset()
+
+            compute.assert_called_once()
 
     def test_generator(self):
-        def checkInTask(test_class):
-            """ Checks correct insertion into the mtc task hierarchy.
-            - The @ p test_class should be able to be inserted as a
-              generator into the task hierarchy.
-            """
-            task = core.Task()
-            task.add(test_class)
-            task.plan()
-
         class extGenerator(core.Generator):
             """ Implements a 'Generator' stage.
             """
 
             def __init__(self, name):
                 core.Generator.__init__(self, name)
-                self.num = 1
+                self.num = self.computeCalls = 3
 
             def reset(self):
-                print('RESET')
+                pass
 
             def canCompute(self):
                 return self.num > 0
 
             def compute(self):
                 self.num -= 1
-                print("compute called")
 
         kwargs = {"name": 'Generator'}
-        gen = extGenerator(kwargs.get('name'))
-        self.check(gen, **kwargs)
+        generator = extGenerator(kwargs.get('name'))
+        self.check(generator, **kwargs)
 
-        # check task insertion
-        checkInTask(gen)
+        with mock.patch.object(extGenerator, 'canCompute', wraps=generator.canCompute) as canCompute, \
+                mock.patch.object(extGenerator, 'compute', wraps=generator.compute) as compute:
+
+            task = core.Task()
+            task.add(generator)
+            task.plan()
+            task.reset()
+
+            canCompute.assert_called()
+
+            # canCompute should be called computeCount * 2 + 1 times:
+            #   - check inside stage itself and it's wrapping container
+            self.assertEqual(
+                canCompute.call_count,
+                generator.computeCalls * 2 + 1)
+
+            compute.assert_called()
+            self.assertEqual(compute.call_count, generator.computeCalls)
 
     @unittest.skip("Monitoring Generator is not yet ready.")
     def test_monitoringGenerator(self):
-        def checkInTask(test_class):
-            """ Checks correct insertion into the mtc task hierarchy
-            - The @ p test_class is tested to be able to ...
-                1. ... be inserted as a monitoring generator stage
-                2. ... execute the setMonitoredStage Method
-            """
-            task = core.Task()
-            task.add(stages.CurrentState('current'))
-
-            # sanity check
-            self.assertEqual(task['current'].name, 'current')
-
-            # add the monitored stage to the monitoring generator
-            test_class.setMonitoredStage(task['current'])
-
-            task.add(test_class)
-            task.plan()
-
         class extMonitoringGenerator(core.MonitoringGenerator):
             """ Implements a 'MonitoringGenerator' stage.
             """
@@ -153,24 +142,27 @@ class TestTrampolines(unittest.TestCase):
                 print('onNewSolution called')
 
         kwargs = {'name': 'MonitoringGenerator'}
-        mgen = extMonitoringGenerator(kwargs.get('name'))
-        self.check(mgen, **kwargs)
+        monitoringGenerator = extMonitoringGenerator(kwargs.get('name'))
+        self.check(monitoringGenerator, **kwargs)
 
-        # check task insertion
-        checkInTask(mgen)
-
-    def test_propagatingEitherWay(self):
-        def checkInTask(test_class):
-            """ Checks correct insertion into the mtc task hierarchy.
-            - The stage @ p test_class should be able to propagate a generator
-              signal into an arbitrary direction.
-            """
+        with mock.patch.object(extMonitoringGenerator, 'onNewSolution', wraps=monitoringGenerator.onNewSolution) as onNewSolution:
 
             task = core.Task()
             task.add(stages.CurrentState('current'))
-            task.add(test_class)
-            task.plan()
 
+            # sanity check
+            self.assertEqual(task['current'].name, 'current')
+
+            # add the monitored stage to the monitoring generator
+            monitoringGenerator.setMonitoredStage(task['current'])
+
+            task.add(monitoringGenerator)
+            task.plan()
+            task.reset()
+
+            onNewSolution.assert_called()
+
+    def test_propagatingEitherWay(self):
         class extPropagatingEitherWay(core.PropagatingEitherWay):
             """ Implements a 'PropagatingEitherWay' stage.
             """
@@ -179,19 +171,43 @@ class TestTrampolines(unittest.TestCase):
                 core.PropagatingEitherWay.__init__(self, name)
 
             def computeForward(self, from_state):
-                print('compute forward')
+                pass
 
             def computeBackward(self, to_state):
-                print('compute backward')
+                pass
 
         kwargs = {'name': 'PropagatingEitherWay'}
-        prop = extPropagatingEitherWay(kwargs.get('name'))
-        self.check(prop, **kwargs)
 
-        # check task insertion
-        checkInTask(prop)
+        propagatingForward = extPropagatingEitherWay(kwargs.get('name'))
+        self.check(propagatingForward, **kwargs)
+
+        propagatingBackward = extPropagatingEitherWay(kwargs.get('name'))
+        self.check(propagatingBackward, **kwargs)
+
+        with mock.patch.object(extPropagatingEitherWay, 'computeForward', wraps=propagatingForward.computeForward) as computeForward, \
+                mock.patch.object(extPropagatingEitherWay, 'computeBackward', wraps=propagatingBackward.computeBackward) as computeBackward:
+
+            # check compute forward
+            task = core.Task()
+            task.add(stages.CurrentState('current'))
+            task.add(propagatingForward)
+            task.plan()
+            task.reset()
+            computeForward.assert_called_once()
+            computeBackward.assert_not_called()
+
+        with mock.patch.object(extPropagatingEitherWay, 'computeForward', wraps=propagatingBackward.computeForward) as computeForward, \
+                mock.patch.object(extPropagatingEitherWay, 'computeBackward', wraps=propagatingBackward.computeBackward) as computeBackward:
+
+            # check compute Backward
+            task = core.Task()
+            task.add(propagatingBackward)
+            task.add(stages.CurrentState('current'))
+            task.plan()
+            task.reset()
+            computeBackward.assert_called_once()
+            computeForward.assert_not_called()
 
 
 if __name__ == '__main__':
-    roscpp_init("test_mtc")
     unittest.main()
